423d9144b9ba342e2e4dd932f55ee8b6753be151
fipelle <6272230+fipelle@users.noreply.github.com>
2021-12-05 05:01:41 +0000

    Added get_data(...)

--- src/CeMicrodata.jl
+++ src/CeMicrodata.jl
@@ -7,9 +7,8 @@ module CeMicrodata
 
     # Custom dependencies
     local_path = dirname(@__FILE__);
-    include("$(local_path)/types.jl");
     include("$(local_path)/get_data.jl");
 
     # Export
-    export QuarterlyDataFrames, get_data;
+    export get_data;
 end
--- src/get_data.jl
+++ src/get_data.jl
@@ -17,37 +17,85 @@ Convert the downloaded csv files of interest (identified via the use of `prefixe
 """
 function csv_files_to_dataframes(survey_id::String, download_folder::String, prefixes::Vector{String})
     
-    # Memory pre-allocation
+    # Memory pre-allocation: sorting problem
     last = "";
-    buffer = Vector{DataFrame}();
-    output = Vector{Vector{NamedTuple}}(undef, length(prefixes));    
+    buffer = Dict{String, DataFrame}();
 
-    # Loop over downloaded csv files
-    for file_name_ext in sort(readdir("$(download_folder)/$(survey_id)"))
+    # Memory pre-allocation: output
+    output = Vector{Dict{String, DataFrame}}(undef, length(prefixes));    
+
+    # Accounts for naming inconsistencies in the folders
+    survey_path = "$(download_folder)/$(survey_id)";
+    readdir_output = sort(readdir(survey_path));
+    if "$(survey_id)" ∈ readdir_output
+        survey_path = "$(download_folder)/$(survey_id)/$(survey_id)";
+        readdir_output = sort(readdir(survey_path));
+    end
+
+    # Loop over the content in `readdir_output` and focus on the csv files
+    for file_name_ext in readdir_output
         file_name = split(file_name_ext, ".")[1];
         file_prefix = file_name[1:end-3];
 
         # Proceed if `file_prefix` is in the target prefixes
         if !isnothing(findfirst(".", file_name_ext)) && (file_prefix ∈ prefixes)
 
+            new_dict_entry = Dict("$(file_name)" => CSV.read("$(survey_path)/$(file_name_ext)", DataFrame));
+
             # Populate `buffer`
             if file_prefix == last
-                push!(buffer, CSV.read("$(download_folder)/$(survey_id)/$(file_name_ext)", DataFrame));
+                merge!(buffer, new_dict_entry);
             
             # New iteration
             else
-                
                 # Populate `output`
                 if length(buffer) > 0
-                    coord_current_file = findfirst(file_prefix .∈ prefixes);
-                    output[coord_current_file] = QuarterlyDataFrames(buffer...);
+                    coord_current_file = findfirst(last .== prefixes);
+                    output[coord_current_file] = copy(buffer);
                     empty!(buffer);
                 end
 
                 # Re-initialise
-                push!(buffer, CSV.read("$(download_folder)/$(survey_id)/$(file_name_ext)", DataFrame));
+                merge!(buffer, new_dict_entry);
                 last = String(file_prefix);
             end
         end
     end
+
+    if length(buffer) > 0
+        coord_current_file = findfirst(last .== prefixes);
+        output[coord_current_file] = copy(buffer);
+    end
+
+    return output;
+end
+
+"""
+    get_data(prefixes::Vector{String}, is_interview_survey::Bool, from_year::Int64, to_year::Int64)
+
+Get data of interest from `from_year` to `to_year`.
+"""
+function get_data(prefixes::Vector{String}, is_interview_survey::Bool, from_year::Int64, to_year::Int64)
+    
+    # Memory pre-allocation: output
+    n_prefixes = length(prefixes);
+    output = Vector{Dict{String, DataFrame}}(undef, n_prefixes);    
+
+    for t=from_year:to_year
+        @info("Downloading survey referring to year $(t)");
+        download_folder = mktempdir(prefix="ce_pumd_", cleanup=true);
+        survey_id = download_csv_files(string(t), is_interview_survey, download_folder);
+        new_entries = csv_files_to_dataframes(survey_id, download_folder, prefixes);
+        for i=1:n_prefixes
+            if isassigned(new_entries, i)
+                if isassigned(output, i)
+                    merge!(output[i], new_entries[i])
+                else
+                    output[i] = new_entries[i];
+                end
+            end
+        end
+    end
+
+    return output;
 end
