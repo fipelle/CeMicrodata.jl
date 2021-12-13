6f864ff4bd178e72293cdd29ccf26c58201eeb19
fipelle <6272230+fipelle@users.noreply.github.com>
2021-12-13 05:01:42 +0000

    Fix in DataFramesMeta implementation

--- src/transform_data.jl
+++ src/transform_data.jl
@@ -96,9 +96,9 @@ function get_hh_level(input_dict::SortedDict{String, DataFrame}; is_itbi::Bool=f
         end
 
         if quarterly_aggregation == false
-            @transform! v_copy @byrow :REF_DATE = Dates.lastdayofmonth(:ref_year, :ref_month);
+            @transform! v_copy @byrow :REF_DATE = Dates.lastdayofmonth(ref_year, ref_month);
         else
-            @transform! v_copy @byrow :REF_DATE = Dates.lastdayofquarter(:ref_year, :ref_month);
+            @transform! v_copy @byrow :REF_DATE = Dates.lastdayofquarter(ref_year, ref_month);
         end
 
         # Construct grouped data
