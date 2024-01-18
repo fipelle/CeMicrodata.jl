507e03c917d54cb66a2314363418fef213088616
fipelle <6272230+fipelle@users.noreply.github.com>
2024-01-18 21:50:08 +0000

    Updated user-agent

--- src/get_data.jl
+++ src/get_data.jl
@@ -7,7 +7,7 @@ function download_csv_files(ref_year::String, is_interview_survey::Bool, downloa
     
     # Setup headers
     headers = Dict(
-        "User-Agent" => "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
+        "User-Agent" => "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
         "Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
         "Accept-Language" => "en-GB,en;q=0.5",
         "Accept-Encoding" => "gzip, deflate, br",
