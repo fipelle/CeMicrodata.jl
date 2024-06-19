6c597bd147302436a51237373711a47cad4d0b47
fipelle <6272230+fipelle@users.noreply.github.com>
2024-06-19 19:44:02 -0300

    new deps

--- src/CeMicrodata.jl
+++ src/CeMicrodata.jl
@@ -3,7 +3,7 @@ __precompile__()
 module CeMicrodata
 
     # Dependencies
-    using CSV, DataFrames, DataFramesMeta, DataStructures, Dates, DelimitedFiles, Downloads, Logging, Statistics;
+    using CSV, DataFrames, DataFramesMeta, DataStructures, Dates, DelimitedFiles, Downloads, FileIO, Logging, Statistics;
 
     # Custom dependencies
     local_path = dirname(@__FILE__);
