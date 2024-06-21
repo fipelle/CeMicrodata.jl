a0c2358229b4f12cc24b73e01e7642a60e8ab13b
fipelle <6272230+fipelle@users.noreply.github.com>
2024-06-21 20:33:07 +0100

    Fix errors in type harmonisation

--- src/get_data.jl
+++ src/get_data.jl
@@ -58,27 +58,25 @@ function harmonize_column_types(data_dict::SortedDict{String, DataFrame})
             # Check the current type of the column
             col_type = eltype(df[!, col]);
             
-            if col_type <: Union{AbstractFloat, AbstractString}
+            if col_type <: Union{AbstractString}
                 try
-                    df[!, col] = convert(Vector{Int64}, df[!, col]);
+                    df[!, col] = parse.(Int64, df[!, col]);
                 catch
                     try
-                        df[!, col] = convert(Vector{Float64}, df[!, col])
+                        df[!, col] = parse.(Float64, df[!, col]);
                     catch
                         df[!, col] = convert(Vector{String}, df[!, col])
                     end
                 end
             
-            elseif col_type <: Union{Union{Missing, AbstractFloat}, Union{Missing, AbstractString}}
+            elseif col_type <: Union{Missing, AbstractString}
+            
+            else
                 try
-                    df[!, col] = convert(Vector{Union{Missing, Int64}}, df[!, col]);
+                    df[!, col] = convert(Vector{Int64}, df[!, col]);
                 catch
-                    try
-                        df[!, col] = convert(Vector{Union{Missing, Float64}}, df[!, col]);
-                    catch
-                        df[!, col] = convert(Vector{Union{Missing, String}}, df[!, col]);
-                    end
-                end            
+                    df[!, col] = convert(Vector{Float64}, df[!, col]);
+                end
             end
         end
     end
